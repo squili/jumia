80a10e325f993a1caca5fab256f32db80a69553c
Julia Johannesen <julia@insertdomain.name>
2022-02-19 23:32:46 -0500

    A little trolling with macros

--- src/client/handler.rs
+++ src/client/handler.rs
@@ -24,28 +24,289 @@ use std::future::Future;
 use std::pin::Pin;
 
 macro_rules! define_event_handler {
-    ($($event_name: ident ($($arg_name: ident : $event_type: ty as $trait_type: ty),*))*) => {
+    ($($tt:tt)*) => {
+        define_event_handler_impl!(@duplicate = {
+            events = [$($tt)*]
+        });
+    };
+}
+
+macro_rules! define_event_handler_impl {
+    // =========================================================================
+    // Duplicate the tokens so we have a reference copy to verify that
+    // $ref == "@ref".
+    // =========================================================================
+    (@duplicate = {
+        events = [$($name:ident($($(@$ref:ident)? $arg:ident: $type:ty),* $(,)?))*]
+    }) => {
+        define_event_handler_impl!(@verify = {
+            events = [$($name($($(@$ref)? $arg: $type),*))*]
+            events_duplicate = [$($name($($(@$ref)? $arg: $type),*))*]
+        });
+    };
+    // =========================================================================
+    // Verify that $@ref == "@ref".
+    // =========================================================================
+    (@verify = {
+        events = [$(
+            $name:ident($(
+                $(@$ref:ident)?
+                $arg:ident:
+                $type:ty
+            ),* $(,)?)
+        )*]
+        events_duplicate = [$(
+            $duplicate_name:ident($(
+                $(@ref)?
+                $duplicate_arg:ident:
+                $duplicate_type:ty
+            ),* $(,)?)
+        )*]
+    }) => {
+        define_event_handler_impl!(@collect_events = {
+            events = [ ]
+            rest = [$($name($($(@$ref)? $arg: $type),*))*]
+        });
+    };
+    // =========================================================================
+    // Collect the events into a structured format that can be emitted easily
+    // =========================================================================
+    (@collect_events = {
+        events = [$({
+            name = $en:ident // event name
+            ref = $eref:literal // event (uses) ref
+            args = [$(
+                // event arg name
+                // event arg original type
+                // event arg referenced type
+                ($ean:ident: $eaot:ty => $eart:ty)
+            )*]
+        })*]
+        rest = [
+            $en_:ident($( // event name
+                $(@$earef_:ident)? // event arg ref
+                $ean_:ident: // event arg name
+                $eat_:ty // event arg type
+            ),* $(,)?)
+            $($rest:tt)*
+        ]
+    }) => {
+        define_event_handler_impl!(@collect_args = {
+            super = {
+                events = [$({
+                    name = $en
+                    ref = $eref
+                    args = [$(($ean: $eaot => $eart))*]
+                })*]
+                rest = [ $($rest)* ]
+            }
+            name = $en_
+            ref = false
+            args = [ ]
+            rest = [ $(($(@$earef_)? $ean_: $eat_))* ]
+        });
+    };
+    (@collect_args = {
+        super = {
+            events = [$({
+                name = $sen:ident // super event name
+                ref = $seref:literal // super event (uses) ref
+                args = [
+                    // super event arg name
+                    // super event arg original type
+                    // super event arg referenced type
+                    $(($sean:ident: $seaot:ty => $seart:ty))*
+                ]
+            })*]
+            rest = [ $($srest:tt)* ] // super rest
+        }
+        name = $en:ident // event name
+        ref = $eref:literal // event (uses) ref
+        args = [
+            // event arg name
+            // event arg original type
+            // event arg referenced type
+            $(($ean:ident: $eaot:ty => $eart:ty))*
+        ]
+        rest = [
+            ($arg:ident: $type:ty)
+            $($rest:tt)*
+        ]
+    }) => {
+        define_event_handler_impl!(@collect_args = {
+            super = {
+                events = [$({
+                    name = $sen
+                    ref = $seref
+                    args = [$(($sean: $seaot => $seart))*]
+                })*]
+                rest = [ $($srest)* ]
+            }
+            name = $en
+            ref = $eref
+            args = [
+                $(($ean: $eaot => $eart))*
+                ($arg: $type => &'__jumia__ &$type)
+            ]
+            rest = [ $($rest)* ]
+        });
+    };
+    (@collect_args = {
+        super = {
+            events = [$({
+                name = $sen:ident // super event name
+                ref = $seref:literal // super event (uses) ref
+                args = [
+                    // super event arg name
+                    // super event arg original type
+                    // super event arg referenced type
+                    $(($sean:ident: $seaot:ty => $seart:ty))*
+                ]
+            })*]
+            rest = [ $($srest:tt)* ] // super rest
+        }
+        name = $en:ident // event name
+        ref = $eref:literal // event (uses) ref
+        args = [
+            // event arg name
+            // event arg original type
+            // event arg referenced type
+            $(($ean:ident: $eaot:ty => $eart:ty))*
+        ]
+        rest = [
+            (@ref $arg:ident: $type:ty)
+            $($rest:tt)*
+        ]
+    }) => {
+        define_event_handler_impl!(@collect_args = {
+            super = {
+                events = [$({
+                    name = $sen
+                    ref = $seref
+                    args = [$(($sean: $seaot => $seart))*]
+                })*]
+                rest = [ $($srest)* ]
+            }
+            name = $en
+            ref = true
+            args = [
+                $(($ean: $eaot => $eart))*
+                ($arg: &$type => &'__jumia__ &$type)
+            ]
+            rest = [ $($rest)* ]
+        });
+    };
+    (@collect_args = {
+        super = {
+            events = [$({
+                name = $sen:ident // super event name
+                ref = $seref:literal // super event (uses) ref
+                args = [
+                    // super event arg name
+                    // super event arg original type
+                    // super event arg referenced type
+                    $(($sean:ident: $seaot:ty => $seart:ty))*
+                ]
+            })*]
+            rest = [ $($srest:tt)* ] // super rest
+        }
+        name = $en:ident // event name
+        ref = $eref:literal // event (uses) ref
+        args = [
+            // event arg name
+            // event arg original type
+            // event arg referenced type
+            $(($ean:ident: $eaot:ty => $eart:ty))*
+        ]
+        rest = [ ]
+    }) => {
+        define_event_handler_impl!(@collect_events = {
+            events = [
+                $({
+                    name = $sen
+                    ref = $seref
+                    args = [$(($sean: $seaot => $seart))*]
+                })*
+                {
+                    name = $en
+                    ref = $eref
+                    args = [$(($ean: $eaot => $eart))*]
+                }
+            ]
+            rest = [ $($srest)* ]
+        });
+    };
+    (@collect_events = {
+        events = [$({
+            name = $name:ident
+            ref = $ref:literal
+            args = [
+                $(($arg_name:ident: $arg_orig_type:ty => $arg_ref_type:ty))*
+            ]
+        })*]
+        rest = [ ]
+    }) => {
+        define_event_handler_impl!(@emit = [$({
+            name = $name
+            ref = $ref
+            args = [
+                $(($arg_name: $arg_orig_type => $arg_ref_type))*
+            ]
+        })*]);
+    };
+    // =========================================================================
+    // Emit the result
+    // =========================================================================
+    (@emit = [$({
+        name = $name:ident
+        ref = $ref:literal
+        args = [
+            $(($arg_name:ident: $arg_orig_type:ty => $arg_ref_type:ty))*
+        ]
+    })*]) => {
         #[derive(Default)]
         pub struct EventHandler {
             $(
-                $event_name: Vec<Box<dyn Fn(&Context, $($trait_type),*) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>>,
+                $name: Vec<Box<
+                    dyn for<'__jumia__> Fn(
+                        &'__jumia__ Context,
+                        $($arg_ref_type),*
+                    ) -> Pin<Box<
+                        dyn Future<Output = ()>
+                        + Send
+                        + '__jumia__
+                    >>
+                    + Send
+                    + Sync
+                >>,
             )*
-            unknown: HashMap<String, Vec<Box<dyn Fn(&Context, &Value) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>>>,
+            unknown: HashMap<String, Vec<Box<
+                dyn for<'__jumia__> Fn(
+                    &'__jumia__ Context,
+                    &'__jumia__ Value,
+                ) -> Pin<Box<
+                    dyn Future<Output = ()>
+                    + Send
+                    + '__jumia__
+                >>
+                + Send
+                + Sync
+            >>>,
         }
 
         #[async_trait]
         impl SerenityEventHandler for EventHandler {
             $(
-                async fn $event_name(&self, ctx: Context, $($arg_name: $event_type),*) {
-                    for callback in &self.$event_name {
-                        callback(&ctx, $(&$arg_name),*).await;
+                async fn $name(&self, ctx: Context, $($arg_name: $arg_orig_type),*) {
+                    for callback in self.$name.iter() {
+                        callback(&ctx, $(&&$arg_name),*).await;
                     }
                 }
             )*
 
             async fn unknown(&self, ctx: Context, name: String, raw: Value) {
                 if let Some(callbacks) = self.unknown.get(&name) {
-                    for callback in callbacks {
+                    for callback in callbacks.iter() {
                         callback(&ctx, &raw).await;
                     }
                 }
@@ -55,11 +316,16 @@ macro_rules! define_event_handler {
         impl EventHandler {
             $(
                 #[allow(unused)]
-                pub fn $event_name<F>(mut self, callback: F) -> Self
+                pub fn $name<F>(mut self, callback: F) -> Self
                 where
-                    F: 'static + Fn(&Context, $($trait_type),*) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync
+                    F: for<'__jumia__> Fn(
+                        &'__jumia__ Context, $($arg_ref_type),*)
+                        -> Pin<Box<dyn Future<Output = ()> + Send + '__jumia__>>
+                            + Send
+                            + Sync
+                            + 'static,
                 {
-                    self.$event_name.push(Box::new(callback));
+                    self.$name.push(Box::new(callback));
                     self
                 }
             )*
@@ -68,73 +334,56 @@ macro_rules! define_event_handler {
 }
 
 define_event_handler! {
-    channel_create(channel: &GuildChannel as &GuildChannel)
-    category_create(channel: &ChannelCategory as &ChannelCategory)
-    category_delete(channel: &ChannelCategory as &ChannelCategory)
-    channel_delete(channel: &GuildChannel as &GuildChannel)
-    channel_pins_update(channel: ChannelPinsUpdateEvent as &ChannelPinsUpdateEvent)
-    channel_update(old: Option<Channel> as &Option<Channel>, new: Channel as &Channel)
-    guild_ban_addition(guild_id: GuildId as &GuildId, banned_user: User as &User)
-    guild_ban_removal(guild_id: GuildId as &GuildId, unbanned_user: User as &User)
-    guild_create(guild: Guild as &Guild, is_new: bool as &bool)
-    guild_delete(incomplete: GuildUnavailable as &GuildUnavailable, full: Option<Guild> as &Option<Guild>)
-    guild_emojis_update(guild_id: GuildId as &GuildId, current_state: HashMap<EmojiId, Emoji> as &HashMap<EmojiId, Emoji>)
-    guild_integrations_update(guild_id: GuildId as &GuildId)
-    guild_member_addition(guild_id: GuildId as &GuildId, new_member: Member as &Member)
-    guild_member_removal(guild_id: GuildId as &GuildId, user: User as &User, member: Option<Member> as &Option<Member>)
-    guild_member_update(old: Option<Member> as &Option<Member>, new: Member as &Member)
-    guild_members_chunk(chunk: GuildMembersChunkEvent as &GuildMembersChunkEvent)
-    guild_role_create(guild_id: GuildId as &GuildId, new: Role as &Role)
-    guild_role_delete(guild_id: GuildId as &GuildId, id: RoleId as &RoleId, role: Option<Role> as &Option<Role>)
-    guild_role_update(guild_id: GuildId as &GuildId, old: Option<Role> as &Option<Role>, new: Role as &Role)
-    guild_unavailable(guild_id: GuildId as &GuildId)
-    guild_update(old: Option<Guild> as &Option<Guild>, new: PartialGuild as &PartialGuild)
-    invite_create(data: InviteCreateEvent as &InviteCreateEvent)
-    invite_delete(data: InviteDeleteEvent as &InviteDeleteEvent)
-    message(message: Message as &Message)
-    message_delete(channel_id: ChannelId as &ChannelId, id: MessageId as &MessageId, guild_id: Option<GuildId> as &Option<GuildId>)
-    message_delete_bulk(channel_id: ChannelId as &ChannelId, ids: Vec<MessageId> as &Vec<MessageId>, guild_id: Option<GuildId> as &Option<GuildId>)
-    message_update(old: Option<Message> as &Option<Message>, new: Option<Message> as &Option<Message>, event: MessageUpdateEvent as &MessageUpdateEvent)
-    reaction_add(reaction: Reaction as &Reaction)
-    reaction_remove(reaction: Reaction as &Reaction)
-    reaction_remove_all(channel_id: ChannelId as &ChannelId, id: MessageId as &MessageId)
-    presence_update(data: PresenceUpdateEvent as &PresenceUpdateEvent)
-    ready(data: Ready as &Ready)
-    resume(data: ResumedEvent as &ResumedEvent)
-    shard_stage_update(data: ShardStageUpdateEvent as &ShardStageUpdateEvent)
-    typing_start(data: TypingStartEvent as &TypingStartEvent)
-    user_update(old: CurrentUser as &CurrentUser, new: CurrentUser as &CurrentUser)
-    voice_server_update(data: VoiceServerUpdateEvent as &VoiceServerUpdateEvent)
-    voice_state_update(guild_id: Option<GuildId> as &Option<GuildId>, old: Option<VoiceState> as &Option<VoiceState>, new: VoiceState as &VoiceState)
-    webhook_update(guild_id: GuildId as &GuildId, channel_id: ChannelId as &ChannelId)
-    interaction_create(interaction: Interaction as &Interaction)
-    integration_create(integration: Integration as &Integration)
-    integration_update(integration: Integration as &Integration)
-    integration_delete(integration_id: IntegrationId as &IntegrationId, guild_id: GuildId as &GuildId, application_id: Option<ApplicationId> as &Option<ApplicationId>)
-    stage_instance_create(stage_instance: StageInstance as &StageInstance)
-    stage_instance_update(stage_instance: StageInstance as &StageInstance)
-    stage_instance_delete(stage_instance: StageInstance as &StageInstance)
-    thread_create(thread: GuildChannel as &GuildChannel)
-    thread_update(thread: GuildChannel as &GuildChannel)
-    thread_delete(thread: PartialGuildChannel as &PartialGuildChannel)
-    thread_list_sync(data: ThreadListSyncEvent as &ThreadListSyncEvent)
-    thread_member_update(thread_member: ThreadMember as &ThreadMember)
-    thread_members_update(thread_members_update: ThreadMembersUpdateEvent as &ThreadMembersUpdateEvent)
-}
-
-impl EventHandler {
-    pub fn unknown<
-        T: Into<String>,
-        F: 'static + Fn(&Context, &Value) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync,
-    >(
-        mut self,
-        name: T,
-        callback: F,
-    ) -> Self {
-        self.unknown
-            .entry(name.into())
-            .or_default()
-            .push(Box::new(callback));
-        self
-    }
+    channel_create(@ref channel: GuildChannel)
+    category_create(@ref category: ChannelCategory)
+    category_delete(@ref category: ChannelCategory)
+    channel_delete(@ref channel: GuildChannel)
+    channel_pins_update(pins: ChannelPinsUpdateEvent)
+    channel_update(old: Option<Channel>, new: Channel)
+    guild_ban_addition(guild_id: GuildId, banned_user: User)
+    guild_ban_removal(guild_id: GuildId, unbanned_user: User)
+    guild_create(guild: Guild, is_new: bool)
+    guild_delete(incomplete: GuildUnavailable, full: Option<Guild>)
+    guild_emojis_update(guild_id: GuildId, current_state: HashMap<EmojiId, Emoji>)
+    guild_integrations_update(guild_id: GuildId)
+    guild_member_addition(guild_id: GuildId, new_member: Member)
+    guild_member_removal(guild_id: GuildId, user: User, member: Option<Member>)
+    guild_member_update(old: Option<Member>, new: Member)
+    guild_members_chunk(chunk: GuildMembersChunkEvent)
+    guild_role_create(guild_id: GuildId, new: Role)
+    guild_role_delete(guild_id: GuildId, id: RoleId, role: Option<Role>)
+    guild_role_update(guild_id: GuildId, old: Option<Role>, new: Role)
+    guild_unavailable(guild_id: GuildId)
+    guild_update(old: Option<Guild>, new: PartialGuild)
+    invite_create(data: InviteCreateEvent)
+    invite_delete(data: InviteDeleteEvent)
+    message(message: Message)
+    message_delete(channel_id: ChannelId, id: MessageId, guild_id: Option<GuildId>)
+    message_delete_bulk(channel_id: ChannelId, ids: Vec<MessageId>, guild_id: Option<GuildId>)
+    message_update(old: Option<Message>, new: Option<Message>, event: MessageUpdateEvent)
+    reaction_add(reaction: Reaction)
+    reaction_remove(reaction: Reaction)
+    reaction_remove_all(channel_id: ChannelId, id: MessageId)
+    presence_update(data: PresenceUpdateEvent)
+    ready(data: Ready)
+    resume(data: ResumedEvent)
+    shard_stage_update(data: ShardStageUpdateEvent)
+    typing_start(data: TypingStartEvent)
+    user_update(old: CurrentUser, new: CurrentUser)
+    voice_server_update(data: VoiceServerUpdateEvent)
+    voice_state_update(guild_id: Option<GuildId>, old: Option<VoiceState>, new: VoiceState)
+    webhook_update(guild_id: GuildId, channel_id: ChannelId)
+    interaction_create(interaction: Interaction)
+    integration_create(integration: Integration)
+    integration_update(integration: Integration)
+    integration_delete(integration_id: IntegrationId, guild_id: GuildId, application_id: Option<ApplicationId>)
+    stage_instance_create(stage_instance: StageInstance)
+    stage_instance_update(stage_instance: StageInstance)
+    stage_instance_delete(stage_instance: StageInstance)
+    thread_create(thread: GuildChannel)
+    thread_update(thread: GuildChannel)
+    thread_delete(thread: PartialGuildChannel)
+    thread_list_sync(data: ThreadListSyncEvent)
+    thread_member_update(thread_member: ThreadMember)
+    thread_members_update(thread_members_update: ThreadMembersUpdateEvent)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![recursion_limit = "256"]
+#![feature(trace_macros)]
+
 pub mod client;
 
 pub use client::Client;
